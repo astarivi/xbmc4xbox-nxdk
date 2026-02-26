43e7b7ef2256e01f1203783609ea1149b488bf24
antonic901 <antonicnikola6@gmail.com>
2026-02-26 16:35:40 +0100

    added: mbed TLS backend

--- xbmc/platform/xbox/main.cpp
+++ xbmc/platform/xbox/main.cpp
@@ -6,37 +6,37 @@
  *  See LICENSES/README.md for more information.
  */
 
-#include "application/AppEnvironment.h"
-#include "application/AppParamParser.h"
-#include "platform/xbmc.h"
+// #include "application/AppEnvironment.h"
+// #include "application/AppParamParser.h"
+// #include "platform/xbmc.h"
 
-#include <nxdk/mount.h>
-#include <nxdk/path.h>
+// #include <nxdk/mount.h>
+// #include <nxdk/path.h>
 
 int main()
 {
-  nxUnmountDrive('D');
-  nxMountDrive('D', "\\Device\\CdRom0");
-  nxMountDrive('C', "\\Device\\Harddisk0\\Partition2\\");
-  nxMountDrive('E', "\\Device\\Harddisk0\\Partition1\\");
-  nxMountDrive('X', "\\Device\\Harddisk0\\Partition3\\");
-  nxMountDrive('Y', "\\Device\\Harddisk0\\Partition4\\");
-  nxMountDrive('Z', "\\Device\\Harddisk0\\Partition5\\");
-  // Mount the root is active xbe to Q:
-  {
-    char targetPath[260];
-    nxGetCurrentXbeNtPath(targetPath);
-    *(strrchr(targetPath, '\\') + 1) = '\0';
-    nxMountDrive('Q', targetPath);
-  }
+  // nxUnmountDrive('D');
+  // nxMountDrive('D', "\\Device\\CdRom0");
+  // nxMountDrive('C', "\\Device\\Harddisk0\\Partition2\\");
+  // nxMountDrive('E', "\\Device\\Harddisk0\\Partition1\\");
+  // nxMountDrive('X', "\\Device\\Harddisk0\\Partition3\\");
+  // nxMountDrive('Y', "\\Device\\Harddisk0\\Partition4\\");
+  // nxMountDrive('Z', "\\Device\\Harddisk0\\Partition5\\");
+  // // Mount the root is active xbe to Q:
+  // {
+  //   char targetPath[260];
+  //   nxGetCurrentXbeNtPath(targetPath);
+  //   *(strrchr(targetPath, '\\') + 1) = '\0';
+  //   nxMountDrive('Q', targetPath);
+  // }
 
-  char* argv[] = {"", "--fullscreen", "--standalone"};
-  CAppParamParser appParamParser;
-  appParamParser.Parse(argv, 3);
+  // char* argv[] = {"", "--fullscreen", "--standalone"};
+  // CAppParamParser appParamParser;
+  // appParamParser.Parse(argv, 3);
 
-  CAppEnvironment::SetUp(appParamParser.GetAppParams());
-  int status = XBMC_Run(true);
-  CAppEnvironment::TearDown();
+  // CAppEnvironment::SetUp(appParamParser.GetAppParams());
+  // int status = XBMC_Run(true);
+  // CAppEnvironment::TearDown();
 
-  return status;
+  return 0;
 }
